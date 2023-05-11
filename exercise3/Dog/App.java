44032ae4afb6f6d72b04ab4b624be57e30745f7d
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-05-11 02:50:09 +0100

    Create App.java

---
+++ exercise3/Dog/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Chowking myPet = new Chowking();
+
+        myPet.bark();
+        System.out.println("this dog's hair color is " + myPet.haircolor);
+        System.out.println("this dog's height is " + myPet.height);
+    }
+}
