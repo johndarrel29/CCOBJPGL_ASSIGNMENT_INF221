a0210e7a750d19a7f2fa28f2d02186c15599e55b
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-05-11 02:48:40 +0100

    Create App.java

---
+++ exercise3/Cat/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        myPet piranhaCAT = new myPet();
+
+        piranhaCAT.meow();
+        
+        System.out.println("This cat's hair color is " + piranhaCAT.haircolor1);
+        System.out.println(piranhaCAT.eyecolor);
+    }
+}
